8618baa2dbf9af674d8fa71fc34462a570dc2449
kukla1989 <kukla.1989@ukr.net>
2022-10-14 16:04:50 +0300

    add language and username to user

---
+++ db/migrate/20221013170323_add_username_and_language_to_users.rb
@@ -0,0 +1,7 @@
+class AddUsernameAndLanguageToUsers < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :username, :string
+    add_index :users, :username, unique: true
+    add_column :users, :language, :string, :default => "ru"
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_11_130741) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_13_170323) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "content"
@@ -35,8 +35,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_11_130741) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
+    t.string "language", default: "ru"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "translated_articles", "articles"
