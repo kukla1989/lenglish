b8f1975ecff6078b3ba7830bdc962f3f6a07825c
kukla1989 <kukla.1989@ukr.net>
2022-10-14 19:35:36 +0300

    create users page

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
+  devise_scope :user do
+    get 'users', :to => 'users/registrations#index'
+  end
+
+
   resources :articles do
     member do
       get :renglish
     end
   end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
   root "articles#index"
 end
