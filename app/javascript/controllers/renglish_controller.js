f49995d595e9f40e3e38d2f2e7648cffd5f726e5
kukla1989 <kukla.1989@ukr.net>
2022-11-01 14:56:58 +0200

    fix small bag in renglish_controller.js

--- app/javascript/controllers/renglish_controller.js
+++ app/javascript/controllers/renglish_controller.js
@@ -4,7 +4,6 @@ export default class extends Controller {
     static targets = ["name"]
 
     connect() {
-        console.log("this works")
         let translated_article = document.getElementById("translated_article").value
         let translated_arr = translated_article.match(/[^\.!\?]+[\.!\?]+["']?|\s*$/g)
         let article_arr = this.name.match(/[^\.!\?]+[\.!\?]+["']?|\s*$/g)
@@ -13,7 +12,7 @@ export default class extends Controller {
             this.arr.push(translated_arr[i])
             this.arr.push(article_arr[i])
         }
-        this.ind = 0
+        this.ind = -1
         while (this.arr[this.arr.length - 1] === "" || this.arr[this.arr.length - 1].length < 5) {
             this.arr.pop()
         }
@@ -21,8 +20,9 @@ export default class extends Controller {
 
     previous() {
         if (this.ind > 0)  {
-            document.getElementById("sentence").innerHTML = this.arr[this.ind]
             this.ind--
+            document.getElementById("sentence").innerHTML = this.arr[this.ind]
+
         }
         if (this.ind === this.arr.length - 2) {
             document.getElementById("last-sentence").style.display = "none"
@@ -32,8 +32,8 @@ export default class extends Controller {
 
     next() {
         if (this.ind < this.arr.length)  {
-            document.getElementById("sentence").innerHTML = this.arr[this.ind]
             this.ind++
+            document.getElementById("sentence").innerHTML = this.arr[this.ind]
         } else {
             document.getElementById("last-sentence").style.display = "block"
         }
